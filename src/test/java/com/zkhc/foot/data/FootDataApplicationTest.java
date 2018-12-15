2e424b084709e66f944828043e645ae961233b83
wuhaisheng <15001167619@163.com>
2018-12-15 10:24:26 +0800

    文件压缩

--- src/test/java/com/zkhc/foot/data/FootDataApplicationTest.java
+++ src/test/java/com/zkhc/foot/data/FootDataApplicationTest.java
@@ -1,6 +1,8 @@
 package com.zkhc.foot.data;
 
 import com.alibaba.fastjson.JSONObject;
+import com.zkhc.foot.data.common.ReportZipUtils;
+import com.zkhc.foot.data.config.constants.SystemConstants;
 import com.zkhc.foot.data.modules.service.IReportService;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
@@ -9,6 +11,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import javax.annotation.Resource;
+import java.io.File;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
@@ -16,6 +19,7 @@ import java.time.LocalTime;
 /**
  * @author 武海升
  * @date 2018/12/6 13:48
+ * https://www.cnblogs.com/DreamDrive/p/5760477.html
  */
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -41,6 +45,27 @@ public class FootDataApplicationTest {
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    @Test
+    public void minusDaysTest() {
+        try {
+            LocalDate localDate = LocalDate.now().minusDays(1);
+            System.out.println(localDate);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    @Test
+    public void zipFile() {
+        try {
+            String zipFilePath = ReportZipUtils.zipFile(new File(SystemConstants.basePath+"数据报告采集_20181208"), "数据报告采集_20181208")
+                .getAbsolutePath();
+            System.out.println(zipFilePath);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
     }
 
 
